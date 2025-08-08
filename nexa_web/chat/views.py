66300cf6656f482b4cebe04621451a362b033ecf
Asset_Dud <maksshirokov48331@yandex.ru>
2025-08-08 17:27:52 +0300

    without time_function

--- nexa_web/chat/views.py
+++ nexa_web/chat/views.py
@@ -6,7 +6,7 @@ GO_SERVER_PORT = "8080"
 
 
 def chat_view(request):
-    client_id = request.GET.get("id", "user")
+    client_id = request.GET.get("id", "sss")
     go_ws_url = f"ws://{GO_SERVER_IP}:{GO_SERVER_PORT}/ws"
     return render(request, 'chat.html', {
         'client_id': client_id,
