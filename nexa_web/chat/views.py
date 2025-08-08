173032683dea05b9db077b3996ce95b8290fbd1f
Asset_Dud <maksshirokov48331@yandex.ru>
2025-08-08 17:19:26 +0300

    add django

---
+++ nexa_web/chat/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+# ⚙️ Укажи IP Go-сервера вручную
+GO_SERVER_IP = "192.168.1.123"
+GO_SERVER_PORT = "8080"
+
+
+def chat_view(request):
+    client_id = request.GET.get("id", "user")
+    go_ws_url = f"ws://{GO_SERVER_IP}:{GO_SERVER_PORT}/ws"
+    return render(request, 'chat.html', {
+        'client_id': client_id,
+        'go_ws_url': go_ws_url,
+    })
